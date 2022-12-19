2ffe5f9352c0c7eadbbf3dbfd5edd48eefc5de76
Gandares <oscar.glez.97@hotmail.com>
2022-12-19 15:56:26 +0000

    Añadida foto para About Me

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="AboutMeContainer">
-        <div id="AboutMeTitle">About Me</div>
+        <div id="AboutMeTitle"><img src="@/static/person-circle.svg" />About Me</div>
+        <div id="AboutMeDescription"><img src="@/static/Positivo.png" />My name is Óscar González García, I am from Spain and I am 25 years old. I am graduated in Computer Engineering and master's degree in Videogame Development at the University of La Laguna. I love the world of computing and I would like to dedicate myself to it, specifically in the field of the master's degree.</div>
     </div>
 </template>
 
@@ -18,6 +19,22 @@
     color: white;
     padding-top: 30px;
     padding-bottom: 30px;
+}
+
+#AboutMeTitle img {
+    width: 30px;
+    height: 30px;
+}
+
+#AboutMeDescription {
+    text-align: justify;
+    background-color: #444;
+    color: white;
+    padding: 50px 5% 50px 5%;
+}
+
+#AboutMeDescription img {
+    width: 40%;
 }
 </style>
 
