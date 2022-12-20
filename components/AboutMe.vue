f867d979f5714fb7cb87a5e1d540356e3469bf47
Gandares <oscar.glez.97@hotmail.com>
2022-12-20 14:15:19 +0000

    Finalizada parte de About Me

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,7 +1,13 @@
 <template>
     <div id="AboutMeContainer">
-        <div id="AboutMeTitle"><img src="@/static/person-circle.svg" />About Me</div>
-        <div id="AboutMeDescription"><img src="@/static/Positivo.png" />My name is Óscar González García, I am from Spain and I am 25 years old. I am graduated in Computer Engineering and master's degree in Videogame Development at the University of La Laguna. I love the world of computing and I would like to dedicate myself to it, specifically in the field of the master's degree.</div>
+        <div id="ImageandTitle">
+            <div class="element" style="width: 37%;"><img id="TitleImage" src="@/static/person-circle.png" /></div>
+            <div class="element" id="AboutMeTitle">About Me</div>
+        </div>
+        <div id="ImageandDescription">
+            <div class="element"><img id="DescriptionImage" src="@/static/Positivo.png" /></div>
+            <div class="element" id="AboutMeDescription">My name is Óscar González García, I am from Spain and I am 25 years old. I am graduated in Computer Engineering and master's degree in Videogame Development at the University of La Laguna. I love the world of computing and I would like to dedicate myself to it, specifically in the field of the master's degree.</div>
+        </div>
     </div>
 </template>
 
@@ -11,30 +17,50 @@
     margin-top: -5px;
 }
 
-#AboutMeTitle {
-    text-align: center;
+#ImageandTitle {
+    display: table;
     width: 100%;
-    font-size: 300%;
+    height: 100%;
+    background-color: #333;
+}
+
+#AboutMeTitle {
+    float: left;
+    font-size: 500%;
     background-color: #333;
     color: white;
     padding-top: 30px;
     padding-bottom: 30px;
 }
 
-#AboutMeTitle img {
-    width: 30px;
-    height: 30px;
+#TitleImage {
+    float: right;
+    width: 15%;
+}
+
+#ImageandDescription {
+    display: table;
+    height: 100%;
+    background-color: #444;
+}
+
+.element {
+    display: table-cell;
+    vertical-align: middle;
 }
 
 #AboutMeDescription {
-    text-align: justify;
-    background-color: #444;
+    margin-left: 10px;
+    text-align: center;
+    font-size: 150%;
+    line-height: 150%;
     color: white;
     padding: 50px 5% 50px 5%;
 }
 
-#AboutMeDescription img {
-    width: 40%;
+#DescriptionImage {
+    width: 100%;
+    padding: 25px 0% 25px 5%;
 }
 </style>
 
