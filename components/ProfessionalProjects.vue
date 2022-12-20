d344475d419bba951a401f8c707854abed72907e
Gandares <oscar.glez.97@hotmail.com>
2022-12-20 16:25:31 +0000

    Añadido titulo proyectos profesionales

--- components/ProfessionalProjects.vue
+++ components/ProfessionalProjects.vue
@@ -1,9 +1,10 @@
 <template>
 <div id="ProfessionalContainer">
     <div id="ProfessionalImageAndTitle">
-        <div class="element" style="width: 37%;"><img id="TitleImage" src="@/static/person-circle.png" /></div>
-        <div class="element" id="AboutMeTitle">Professional Projects</div>
+        <div class="element" style="width: 23%;"><img id="TitleImageProfessional" src="@/static/person-circle.png" /></div>
+        <div class="element" id="ProfessionalTitle">Professional Projects</div>
     </div>
+    <div></div>
 </div>
 </template>
 
@@ -14,8 +15,27 @@
 }
 
 #ProfessionalImageAndTitle {
-
+    display: table;
+    width: 100%;
+    height: 100%;
+    background-color: #333;
 }
+
+#ProfessionalTitle {
+    float: left;
+    font-size: 400%;
+    background-color: #333;
+    color: white;
+    padding-top: 30px;
+    padding-bottom: 30px;
+}
+
+#TitleImageProfessional {
+    float: right;
+    width: 20%;
+}
+
+
 </style>
 
 <script>
